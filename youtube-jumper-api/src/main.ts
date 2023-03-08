f6ea0509e4e48f597e25ab48460c933a4a2a30be
Rogerson Nazário <rrnazario@gmail.com>
2023-03-08 14:10:32 +0100

    [BE] Add cors

--- youtube-jumper-api/src/main.ts
+++ youtube-jumper-api/src/main.ts
@@ -3,6 +3,13 @@ import { IntervalModule } from './interval.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(IntervalModule);
+  app.enableCors({
+    origin: [
+      process.env.CORS_URL,
+    ],
+    methods: ["GET", "PUT"],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 8558);
 }
 
